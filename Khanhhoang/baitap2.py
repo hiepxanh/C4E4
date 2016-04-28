722eec0d8510fb2b665ae537d432509f711cbdc6
Hoang Nguyen <khanhhoang.adt@gmail.com>
2016-04-28 16:20:10 +0700

    update baitap2

--- Khanhhoang/baitap2.py
+++ Khanhhoang/baitap2.py
@@ -1,3 +1,7 @@
+#check a variable's type : type(x) --> <class ''>
+#3 different examples of invalid names: 1hoang
+#                                       hoang#
+#                                       from
 from turtle import*
 # 2 Write a program that calculates the ​ area​ of a ​ circle
 radius = int(input('radius :'))
@@ -12,3 +16,12 @@ b = int(input('initial B bacteria number :'))
 time = int(input('time :'))
 b_later = b*(2**(time//2))
 print(b_later)
+#5 Happy Farm
+m1=1
+m2=2
+month=int(input('month :'))
+for month in range(month):
+    temp = m1
+    m1=m2
+    m2=temp+b
+print(m2)
