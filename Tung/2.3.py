c1a4718055b19b19183d7096d65050fc35a4e80a
tungtrandt13 <tungtrandt13@gmail.com>
2016-05-03 18:06:47 +0700

    Added files via upload

---
+++ Tung/2.3.py
@@ -0,0 +1,8 @@
+list=[5,7,300,90,24,50,75]
+print('Hello, my name is Tung and these are my ship sizes')
+print(list)
+print("Now my biggest sheep has size ",max(list),"let 's shear it")
+list.append(8)
+list.remove(max(list))
+print("After shearing,here is my flock")
+print(list)
