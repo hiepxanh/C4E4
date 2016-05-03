b3520d6f18d900fce2eeead73224d16da7e4a047
Mai Huong <maihuongcmt@gmail.com>
2016-05-03 08:53:59 +0700

    Assignment - Session 03

---
+++ MaiHuong/Session 03 - Assignment 02 (flock sheep).py
@@ -0,0 +1,22 @@
+
+# Exercise 2: flock of sheeps
+from turtle import *
+sizes = [5, 7, 300, 90, 24, 50, 75] ## flock sizes
+print("Hello, my name is Hiep and these are my sheep sizes ", sizes)
+n = int(input("Nhap thang can thu hoach: "))   
+for t in range(1, n+1):
+    print("MONTH ", t, ":")
+    for i in range (len(sizes)):
+        sizes[i] = sizes[i] + 50
+    print("One month has passed and here is my flock", sizes)
+    if t == n:
+        print("Flock has sizes in total: ", sum(sizes))
+        print(str.format("I would has {0} * 2$ = {1}", sum(sizes), sum(sizes) * 2))
+    else:
+        print("Now my biggest sheep has size", max(sizes), "let's sheer it")
+        max_size_index = sizes.index(max(sizes))
+        sizes[max_size_index] = 8
+        print("After shearing, here is my flock", sizes)
+
+mainloop()
+    
