730b5f4db18648c1a1cbb7a12b0cb3f635419f34
DauMinhHoa <hoa.dauminh@gmail.com>
2016-05-05 12:49:13 +0700

    Added files via upload
    mình làm chưa hết, khó hiểu

---
+++ DauMinhHoa/W2-as3-Sheep.py
@@ -0,0 +1,8 @@
+##2.1    
+sizes = [5,7,300,90,24,50,75]
+print('Hello, my name is hiep and these are my sheep sizes:')
+print(sizes)
+##2.2
+m=max(sizes)
+print("Now my biggest sheep has size",m, "let's shear it !")
+##Tu 2.3 chiu
