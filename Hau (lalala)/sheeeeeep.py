94de4a220ea6480758f9f0147f3d624dcd90c34e
Nguyen Hai Hau <nguchobotsau@gmail.com>
2016-05-06 12:44:31 +0700

    bị kêu là ẩu....

--- Hau (lalala)/sheeeeeep.py
+++ Hau (lalala)/sheeeeeep.py
@@ -1,19 +1,18 @@
-a=input('enter number of sheeps:')
-a=int(a)
-size=[]
-flock=0
-print('Size plz:')
-for x in range(a):
-    size.append(int(input(str(x)+':')))
-print('what u have just entered is: ')
+size=[5,7,300,90,24,50,75]
+print('Hello, my name is Hau and here is my flock: ')
 print(size)
+flock=0
+size.sort()
+print('now my largest sheep has size of ', max(size))
 for y in range(4):
     size.sort()
-    print('largest is ', max(size))
+    print('now after '+str(y+1)+' month my largest sheep has size of '+str(max(size)))
     flock=flock+max(size)
-    del size[len(size)-1]
     print(size)
+    size.append(8)
+    del size[len(size)-2]
     for x in range (len(size)):
         size[x]=size[x]+50
-print('you got '+ str(flock*3)+' after 4 month by '+str(flock))
+    
+print('you got '+str(flock)+'*2$='+str(flock*2)+ ' after 4 month by '+str(flock)+' flock')
 
