8bb409bd446d18a4c0f1ba128c4ff1cab3f96519
khanhnth <nghoakhanh89@gmail.com>
2016-05-10 16:57:20 +0700

    edit bai tap 5
    them phan write new file

--- HoaKhanh/Session 5 - assignment 5.py
+++ HoaKhanh/Session 5 - assignment 5.py
@@ -1,7 +1,10 @@
 file = open('ss5_data.txt')
+new = open('new_file.txt','w')
 for line in file:
-##    print(line)
     l = line.strip().split(" ")
     wage = int(l[1])*int(l[2])
-    l.append(wage)
-    print(l)
+    l.append(str(wage))
+    l_new = str(l)
+    new.write(l_new+'\n')
+new.close()
+    
