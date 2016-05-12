e2fcb79aadf8eaef4274b9728a9b5f060f7dd230
PKhoamai <bichphuong_2508@yahoo.com>
2016-05-12 18:05:55 +0700

    PKhoamai
    Revise HW ^^

--- HW session 6.py
+++ HW session 6.py
@@ -13,7 +13,7 @@ inventory["gold"]=inventory["gold"]+50
 print(inventory)
 #EX2
 price={"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
-stock={"banana": 6,"apple": 2,"orange": 32,"pear": 3}
+stock={"banana": 6,"apple": 0,"orange": 32,"pear": 3}
 def gia(x):
         for x,i in price.items():
                return i
@@ -22,7 +22,7 @@ def ton(y):
                 return j
 
 print("in my store:\n")
-def compute_bill(price,stock):
+def compute_bill(x):
         total =0
         for x,i in price.items():                
                 for y,j in stock.items():
@@ -38,8 +38,31 @@ for x,i in price.items():
                                  print("\n stock: ",j)                             
         
 print("\n if we sold all of them we will earn totally is: ")
-print(compute_bill(price,stock))
+print(compute_bill(x))
 #3:
-
+price={"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
+stock={"banana": 6,"apple": 0,"orange": 32,"pear": 3}
 groceries = ["banana","orange", "apple"]
+print ('in Lulu store have: ',stock)
+print ('all the price is: ',price)
+print( 'if you buy this: ',groceries)
+
+def bill(foodlist):
+        total = 0
+        for i in foodlist:
+                if i in stock and stock[i]>1:            
+                    print('you bought',i)
+                    total += price[i]
+                    stock[i] = stock[i] - 1
+                else:
+                        print(i, ": your food you chose is not available")
+        print('your total expense is:',total)
+        print('remaining stuff in Lulu store is:',stock)
+        return total
+        return stock
+
+print("your total spend is: ", total)
+print("remain stuff in Lulu store is: ", stock)
+bill(groceries)
+
 
