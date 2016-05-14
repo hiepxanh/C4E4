3d6a28029dfb0561340c00daa83e998cf804b0d3
Hoanh An <hoanhan101@gmail.com>
2016-05-14 13:31:46 +0700

    SS7_AS_updated

--- hoanhan101/ss7_as.py
+++ hoanhan101/ss7_as.py
@@ -10,22 +10,30 @@ menu_collection = db["menu"]
 orders_collection = db["orders"]
 
 # EX3
-# all_menu = menu_collection.find()
-# for x in all_menu:
-#     print(x)
-# all_menu = menu_collection.find()
+
+all_menu = menu_collection.find()
 # for x in all_menu:
 #     print(x["name"], ":", x["price"])
 
 # EX4
-# orders_collection.insert_one({"customer":"hoanh","items":["white coffee","cookies"]})
-# all_orders = orders_collection.find()
+# orders_collection.insert_one({"customer":"hoanh","items":["chips","red tea"]})
+all_orders = orders_collection.find()
 # for i in all_orders:
 #     print(i["customer"],":",i["items"])
 
 # EX5
-all_orders = orders_collection.find_one({"customer":"hiep"})
-print("Hiep's order:",all_orders["items"])
-bill_chip = menu_collection.find_one({"name":"chips"})
-bill_black_coffee = menu_collection.find_one({"name":"black coffee"})
-print("Hiep has to pay:",bill_black_coffee["price"]+bill_chip["price"])
+u = input("Whose bill:")
+
+customer_order = orders_collection.find_one({"customer":u})
+print(u,"order: ",customer_order["items"])
+
+total = 0
+for menu in all_menu:
+    for order in customer_order["items"]:
+        # for menu in all_menu:
+        if order == menu["name"]:
+            total = total + menu["price"]
+print(u,"has to pay:",total)
+
+
+
