c9a1b576b4843375e7fd2204e8fd7eb77b8090b0
Hoanh An <hoanhan101@gmail.com>
2016-05-23 22:29:52 +0700

    lab4_ninja

---
+++ hoanhan101/lab4_ninja.py
@@ -0,0 +1,50 @@
+class Ninja:
+    def __init__(self,name,village,healthPoint,attackPoint,defensePoint):
+        self.name = name
+        self.village = village
+        self.healthPoint = healthPoint
+        self.attackPoint = attackPoint
+        self.defensePoint = defensePoint
+    def attack(self,enemy):
+        ninja_name = self.name
+        enemy_name = enemy.name
+        ninja_village = self.village
+        enemy_village = enemy.village
+        ninja_hp = self.healthPoint
+        enemy_hp = enemy.healthPoint
+        ninja_atk = self.attackPoint
+        enemy_atk = enemy.attackPoint
+        ninja_def = self.defensePoint
+        enemy_def = enemy.defensePoint
+
+        if ninja_atk < enemy_def:
+            print(ninja_name,"can not attack",enemy_name)
+            # print("\t",ninja_name,"HP =",ninja_hp)
+            print("\t" + enemy_name + "'s HP is still " + str(enemy_hp))
+        else:
+            enemy_hp = enemy_hp - (ninja_atk - enemy_def)
+            print(ninja_name,"attacks",enemy_name)
+            # print("\t",ninja_name,"HP =",ninja_hp)
+            print("\t" + enemy_name + "'s remaining HP is " + str(enemy_hp))
+        return enemy_hp
+
+naruto = Ninja("Naruto","Leaf",11,7,4)
+gaara = Ninja("Gaara","Sand",12,6,8)
+zabuza = Ninja("Zubuza","Fog",9,8,3)
+deidara = Ninja("Deidara","Rock",8,10,1)
+
+
+print("Before Combat:")
+print("\t" + naruto.name + "'s HP is " + str(naruto.healthPoint))
+print("\t" + gaara.name + "'s HP is " + str(gaara.healthPoint))
+print("\t" + zabuza.name + "'s HP is " + str(zabuza.healthPoint))
+print("\t" + deidara.name + "'s HP is " + str(deidara.healthPoint))
+
+print("")
+
+print("In Combat:")
+naruto.attack(gaara)
+zabuza.attack(naruto)
+gaara.attack(deidara)
+deidara.attack(zabuza)
+naruto.attack(zabuza)
