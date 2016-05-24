ee9f53f07194b5687adc1deaa56b9305ce0e1b4e
PKhoamai <bichphuong_2508@yahoo.com>
2016-05-24 11:08:31 +0700

    HW

--- PKhoamai/Lab 4 assignment PK.py
+++ PKhoamai/Lab 4 assignment PK.py
@@ -8,10 +8,12 @@ class Ninja:
     def attack(self,enemy):
         if self.attackpoint > enemy.defensepoint:
             enemy.healthpoint = enemy.healthpoint -(self.attackpoint - enemy.defensepoint)
+            print(self.name, ": ", self.healthpoint, "\n", enemy.name, ": ", enemy.healthpoint)
             return enemy.healthpoint
+
         else:
-            return enemy.healthpoint
-        print(self.name, ": ", self.healthpoint,"\n",enemy.name, ": ", enemy.healthpoint)
+            print(self.name, ": ", self.healthpoint, "\n", enemy.name, ": ", enemy.healthpoint)
+
 
 
 Naruto = Ninja("Naruto", "Leaf", 11, 7, 4)
@@ -19,21 +21,17 @@ Gaara = Ninja("Gaara", "Sand", 12, 6, 8)
 Zabuza = Ninja("Zabuza", "Fog", 9, 8, 3)
 Deidara = Ninja("Deidara", "Rock", 8, 10, 1)
 
-Naruto.attack(Gaara)
-Zabuza.attack(Naruto)
-Gaara.attack(Deidara)
-Deidara.attack(Zabuza)
+# Naruto.attack(Gaara)
+#
+# Zabuza.attack(Naruto)
+#
+# Gaara.attack(Deidara)
+#
+# Deidara.attack(Zabuza)
 
-# while True:
-#     Naruto.attack(Gaara)
-#     if Gaara.healthpoint<=0:
-#         break
-#     Zabuza.attack(Naruto)
-#     if Naruto.healthpoint <=0:
-#         break
-#     Gaara.attack(Deidara)
-#     if Deidara.healthpoint <=0:
-#         break
-#     Deidara.attack(Zabuza)
-#     if Zabuza.healthpoint <=0:
-#         break
+for x in range (4):
+    Naruto.attack(Gaara)
+    Zabuza.attack(Naruto)
+    Gaara.attack(Deidara)
+    Deidara.attack(Zabuza)
+
