5518e6bcc384b92fa84451f77cab348ed7e05c6b
Hoanh An <hoanhan101@gmail.com>
2016-05-25 11:59:26 +0700

    SS10_AS_SCRAPING_VIETNAMNET

---
+++ hoanhan101/ss10_as_scraping_vietnamnet.py
@@ -0,0 +1,28 @@
+from urllib import request
+import bs4
+import pymongo
+
+db_uri = "mongodb://c4e:codethechange@ds011893.mlab.com:11893/c4e_rss"
+
+db = pymongo.MongoClient(db_uri).get_default_database()
+db_posts = db["posts"]
+
+link = "http://vietnamnet.vn"
+
+r = request.urlopen(link)
+html_source = r.read()
+print(html_source)
+
+soup = bs4.BeautifulSoup(html_source, "html.parser")
+
+posts = soup.find("ul", {"class":"HotList"})
+for post in posts:
+    link = post.h2.a["href"]
+    title = "hoanhan101: " + post.h2.a.string
+    db_posts.insert_one(
+        {
+            "title:":title,
+            "link": link
+        }
+    )
+
